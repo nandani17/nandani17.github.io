ae25bcede4f5733a92a714bd97ffdaebd750cac7
nandani17 <raj17nandani@gmail.com>
2017-01-29 16:25:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function open_card() {
   document.getElementById('outside').className = 'open-card';
-}
+
+ 
+       var audio = document.getElementById("audio");
+       audio.play();
+                 }
 
 
 function close_card() {
