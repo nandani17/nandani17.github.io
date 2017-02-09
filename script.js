418f33bff1654c20bcdd7e8c78a430aff4013df2
nandani17 <raj17nandani@gmail.com>
2017-02-09 20:10:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,48 @@ function open_card() {
                  }
 
 
+
+function open_card11() {
+  document.getElementById('mid1').className = 'open-card';
+
+} 
+
+
+function open_card12() {
+  document.getElementById('mid2').className = 'open-card';
+
+} 
+
+
+function open_card13() {
+  document.getElementById('mid3').className = 'open-card';
+
+} 
+
+
+function open_card14() {
+  document.getElementById('mid4').className = 'open-card';
+
+} 
+
+
+function open_card15() {
+  document.getElementById('mid5').className = 'open-card';
+
+} 
+
+
+function open_card16() {
+  document.getElementById('mid6').className = 'open-card';
+
+} 
+
+function open_card17() {
+  document.getElementById('mid7').className = 'open-card';
+
+}
+
+
 function close_card() {
   document.getElementById('outside').className = '';
 }
